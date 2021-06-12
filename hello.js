0122f35b2c92fcc369d69b960484acd50db7971b
zish <zish4u@gmail.com>
2021-06-12 21:10:57 +0530

    Random javascript code.

--- hello.js
+++ hello.js
@@ -1,1 +1,22 @@
 console.log("Hello World!")
+
+//Removing duplicate value from an array.
+var arr = [1, 2, 2, 3, 9, 7, 8, 9]
+function removeDuplicate() {
+    var unique = []
+    arr.forEach(value => {
+        if (!unique.includes(value)) {
+            unique.push(value)
+        }
+    })
+    return unique
+}
+console.log("Duplicate Arr: ", removeDuplicate())
+
+var x = {
+    name: 'ramesh'
+}
+var y = x
+y.name = 'Zishan'
+
+console.log(x.name)
