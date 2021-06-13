c83d96cd181f6ecd342ae0f675a7cd7b1ac113a0
zish <zish4u@gmail.com>
2021-06-13 11:39:46 +0530

    Assignement done for first three lectures.

---
+++ 01-Fundamentals-Part-1/assignment.js
@@ -0,0 +1,27 @@
+// LECTURE: Values and Variables
+
+const myCountry = 'India';
+const myContinent = 'Asia';
+let populationInMilions = 1378.6;
+// console.log(myCountry)
+// console.log(myContinent)
+// console.log(populationInMilions)
+
+
+// LECTURE: Data Types
+const isIsland = false
+let language;
+console.log(typeof myCountry)
+console.log(typeof myContinent)
+console.log(typeof populationInMilions)
+console.log(typeof isIsland)
+console.log(typeof language)
+
+// LECTURE: let,const and var
+language = 'Hindi'
+myContinent = 'Africa'
+myCountry = 'England'
+isIsland = true
+console.log(myContinent)
+console.log(myCountry)
+console.log(isIsland)
--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,5 +1,19 @@
+/*
 let js = 'amazing'
 if (js === 'amazing') alert('JavaScript is FUN!')
 console.log(40 + 8 + 23 - 10)
 console.log('Zishan')
 console.log(23)
+*/
+
+// let javascriptIsFun = true
+// let undefinedValueType;
+// console.log(javascriptIsFun)
+// console.log(typeof javascriptIsFun)
+// console.log(typeof true)
+// console.log(typeof 'Zishan')
+// console.log(typeof 23)
+// console.log(typeof undefinedValueType)
+// javascriptIsFun = 'Yes!'
+// console.log(typeof javascriptIsFun)
+// console.log(typeof null)
