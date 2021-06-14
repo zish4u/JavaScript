d51d4c425cc18472cb9538234599ca45a0aa3365
zish <zish4u@gmail.com>
2021-06-14 23:49:03 +0530

    Youtube course practice code added.

--- 01-Fundamentals-Part-1/challenges.js
+++ 01-Fundamentals-Part-1/challenges.js
@@ -15,8 +15,8 @@ johnBMI = johnMass / (johnHeight * johnHeight)
 
 // Coding Challenge #2
 
-if (markBMI > johnBMI) {
-    console.log(`Mark's BMI(${markBMI}) is higher than John's(${johnBMI})!`)
-} else {
-    console.log(`John's BMI(${johnBMI}) is higher than Mark's(${markBMI})!`)
-}
+// if (markBMI > johnBMI) {
+//     console.log(`Mark's BMI(${markBMI}) is higher than John's(${johnBMI})!`)
+// } else {
+//     console.log(`John's BMI(${johnBMI}) is higher than Mark's(${markBMI})!`)
+// }
---
+++ YoutubeCourse/practice.js
@@ -0,0 +1,17 @@
+//true+true
+//"string"-"string"
+// 9-"5"
+//false-true
+
+// console.log(true + true)
+// console.log("string" - "string")
+// console.log(9 - "5")
+// console.log(false - true)
+// console.log(isNaN(9534))
+// console.log(isNaN('string'))
+console.log(NaN === NaN)
+console.log(Number.NaN === NaN)
+console.log(Number.NaN === Number.NaN)
+console.log(isNaN(NaN))
+console.log(isNaN(Number.NaN))
+console.log(Number.isNaN(NaN))
